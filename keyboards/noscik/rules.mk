2200ac193985d79baf742c625a035e27be3df9cb
Jocelyn Boullier <jocelyn@boullier.bzh>
2024-08-07 08:49:23 +0200

    revert to colemak

--- keyboards/noscik/rules.mk
+++ keyboards/noscik/rules.mk
@@ -2,6 +2,8 @@ SERIAL_DRIVER = vendor
 DEFERRED_EXEC_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+# UNICODE_COMMON = true
 
 SRC += features/caps_word.c
 SRC += features/custom_shift_key.c
